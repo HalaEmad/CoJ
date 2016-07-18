d79fc336f7844e9f417238e4658633b925723a4e
emanhassan <eman.hassan@eg.ibm.com>
2016-07-18 12:50:27 +0200

    #Eman finishing walkthrough

---
+++ app/src/main/java/com/ir/android/walkthrough/WalkthroughAdapterCtrl.java
@@ -0,0 +1,14 @@
+package com.ir.android.walkthrough;
+
+import com.ibm.android.kit.controllers.Controller;
+import com.ibm.android.kit.models.ViewModel;
+
+/**
+ * Created by emanhassan on 7/18/16.
+ */
+public class WalkthroughAdapterCtrl extends Controller{
+    @Override
+    public ViewModel initViewModel() {
+        return null;
+    }
+}
