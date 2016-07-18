2b673c8be3b126be0bea360d04b7d8f7805f2d76
HalaEmad <eng_hala2008@hotmail.com>
2016-07-19 00:33:35 +0200

    updated worklight adapters

--- CoJ/adapters/Authentication/Authentication-impl.js
+++ CoJ/adapters/Authentication/Authentication-impl.js
@@ -4,22 +4,19 @@ var _Constants = {
    REALM : "IOCAuthRealm",
    LTPATOKEN : "LtpaToken2"
 };
-/*auth will be the encoded value base64 of username and password like hala:1234
-so user name is hala & password is 1234
-*/
 function authenticateUser(auth) {
 var input = {
 method : 'get',
-returnedContentType : 'application/json', 
-path : "/ibm/ioc/api/login-service/users", 
-headers : {
+returnedContentType : 'application/json',
+path : "/ibm/ioc/api/login-service/users", headers : {
          Authorization : "Basic " + auth,
          Accept : "application/json"
       }
 };
-// get the JSESSIONID out of the request header 
+// get the JSESSIONID out of the request header
 var request = WL.Server.getClientRequest();
-var cookieHeader = request.getHeader("Cookie"); var cookies = cookieHeader.split(";");
+var cookieHeader = request.getHeader("Cookie"); 
+var cookies = cookieHeader.split(";");
 var jSessionId = null;
 for ( var i = 0; i < cookies.length; i++) {
 var cookie = cookies[i];
@@ -30,31 +27,35 @@ if (cookie.indexOf(_Constants.JSESSIONID) >= 0) {
          break;
 } }
 if (jSessionId) {
-input.headers[_Constants.SESSIONID] = jSessionId; 
-input.headers["Cookie"] = _Constants.JSESSIONID + "=" + jSessionId;
+input.headers[_Constants.SESSIONID] = jSessionId; input.headers["Cookie"] = _Constants.JSESSIONID + "=" + jSessionId;
    }
    var result = WL.Server.invokeHttp(input);
 // if successful, set active user
 if (result.statusCode && result.statusCode == 200 && result.array) {
-var user = JSON.parse(JSON.stringify(result.array[0])); // clone user.base64 = auth;
+var user = JSON.parse(JSON.stringify(result.array[0])); // clone 
+user.base64 = auth;
 user.sessionId = null;
 user.ltpaToken = null;
 if (result.responseHeaders) {
 var responseHeaders = result.responseHeaders;
 // process the headers
 user.sessionId = responseHeaders[_Constants.SESSIONID];
+
 // process the cookies
 var setCookieHeader = responseHeaders["Set-Cookie"]; 
+if(setCookieHeader!=null){
 var cookieData = setCookieHeader.split(";");
-
 for ( var i = 0; i < cookieData.length; i++) {
               var cookie = cookieData[i];
-              if (cookie.indexOf(_Constants.LTPATOKEN) > -1) {
+              if (cookie.indexOf(_Constants.LTPATOKEN) > -1)
+   {
 var valuePattern = /[=].*/;
-var value = "" + valuePattern.exec(cookie); user.ltpaToken = value.trim().substr(1); // strip the // leading "="
+var value = "" + valuePattern.exec(cookie);
+user.ltpaToken = value.trim().substr(1); // strip the  leading "="
 break;
 } }
 }
+
          var userIdentity = {
             userId : user.uid,
             attributes : user
@@ -69,4 +70,9 @@ WL.Server.setActiveUser(_Constants.REALM, null); return {
 statusMsg: 'authentication failed, check logs for detailed error', statusCode : result.statusCode,
 userIdentity : {}
 }; }
+}
+}
+function logoutUser()
+{
+	WL.Server.setActiveUser(_Constants.REALM, null);
 }
